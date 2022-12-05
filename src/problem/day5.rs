4505295a91f9a76aff7f272ec89669dbd5546345
Tomas Haddad <tomashaddad@gmail.com>
2022-12-05 20:51:42 +1100

    Complete Day 5 Part 2

--- src/problem/day5.rs
+++ src/problem/day5.rs
@@ -22,43 +22,47 @@ struct Instruction {
     to: usize,
 }
 
+fn parse(input: &str) -> (Vec<VecDeque<char>>, Vec<Instruction>) {
+    let state = transpose(
+        input
+            .lines()
+            .take_while(|line| !line.is_empty())
+            .map(|line| line.chars().collect())
+            .collect(),
+    );
+
+    let state = state
+        .into_iter()
+        .map(|row| VecDeque::from(row))
+        .filter(|row| row.back().unwrap().is_numeric())
+        .map(|mut row| {
+            row.pop_back();
+            row.retain(|c| !c.is_whitespace());
+            VecDeque::from(row)
+        })
+        .collect::<Vec<_>>();
+
+    let instructions = input
+        .lines()
+        .skip_while(|line| !line.starts_with("move"))
+        .map(|line| {
+            let mut parts = line
+                .split_whitespace()
+                .filter_map(|s| s.parse::<usize>().ok());
+
+            Instruction {
+                amount: parts.next().unwrap(),
+                from: parts.next().unwrap(),
+                to: parts.next().unwrap(),
+            }
+        })
+        .collect::<Vec<_>>();
+    (state, instructions)
+}
+
 impl Day for Code {
     fn part1(&self, input: &str) -> String {
-        let state = transpose(
-            input
-                .lines()
-                .take_while(|line| !line.is_empty())
-                .map(|line| line.chars().collect())
-                .collect(),
-        );
-
-        let mut state = state
-            .into_iter()
-            .map(|row| VecDeque::from(row))
-            .filter(|row| row.back().unwrap().is_numeric())
-            .map(|mut row| {
-                row.pop_back();
-                row.retain(|c| !c.is_whitespace());
-                VecDeque::from(row)
-            })
-            .collect::<Vec<_>>();
-
-        let instructions = input
-            .lines()
-            .skip_while(|line| !line.starts_with("move"))
-            .map(|line| {
-                let mut parts = line
-                    .split_whitespace()
-                    .filter_map(|s| s.parse::<usize>().ok());
-
-                Instruction {
-                    amount: parts.next().unwrap(),
-                    from: parts.next().unwrap(),
-                    to: parts.next().unwrap(),
-                }
-            })
-            .collect::<Vec<_>>();
-
+        let (mut state, instructions) = parse(input);
         instructions.iter().for_each(|i| {
             for _ in 0..i.amount {
                 let value = state[i.from - 1].pop_front().unwrap();
@@ -70,6 +74,18 @@ impl Day for Code {
     }
 
     fn part2(&self, input: &str) -> String {
-        todo!();
+        let (mut state, instructions) = parse(input);
+        instructions.iter().for_each(|i| {
+            let mut values: VecDeque<char> = VecDeque::new();
+            for _ in 0..i.amount {
+                values.push_front(state[i.from - 1].pop_front().unwrap());
+            }
+
+            for value in values {
+                state[i.to - 1].push_front(value);
+            }
+        });
+
+        state.iter().map(|s| s[0]).collect()
     }
 }
